acb5da4ad7c391d6e205d40b2a678975e3c0bdcc
Matt Winzer <matt.winzer@gmail.com>
2019-01-15 12:39:52 -0700

    added form-data package, constructed formData object to post to imgur

--- routes/images.js
+++ routes/images.js
@@ -4,14 +4,28 @@ const controller = require('../controllers/imagesController')
 const multer = require('multer')
 const upload = multer()
 const fetch = require('node-fetch')
+const FormData = require('form-data')
+const fs = require('fs')
+
+const imgurApi = 'https://api.imgur.com/3/image'
 
 router.get('/', controller.imageTest)
 
 router.post('/upload', upload.single('image'), (req, res, next) => {
   const file = req.file
   const body = req.body
-  console.log('file', file)
-  console.log('body', body)
+
+  // console.log(file)
+
+  let form = new FormData()
+  form.append('image', file.buffer)
+  form.append('title', req.body.title)
+  form.append('description', 'An image uploaded through my proxy server')
+  form.append('name', req.file.originalname)
+
+  console.log(form)
+  fetch()
+
   res.json({ msg: 'Trying!' })
 })
 
